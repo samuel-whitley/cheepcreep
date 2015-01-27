a9ba350a723be4dd27411b2efe0d5224e5de724f
samuel <samuel.whitley89@gmail.com>
2015-01-27 18:57:01 -0500

    added star,list,unstar, and a broken delete

--- lib/cheepcreep.rb
+++ lib/cheepcreep.rb
@@ -11,35 +11,55 @@ end
 class Github
   include HTTParty
   base_uri 'https://api.github.com'
+  basic_auth ENV['GITHUB_USER'], ENV['GITHUB_PASS']
+
 
   def initialize
     # ENV["FOO"] is like echo $FOO
-    @auth = {:username => ENV['GITHUB_USER'], :password => ENV['GITHUB_PASS']}
+    #@auth = {:username => ENV['GITHUB_USER'], :password => ENV['GITHUB_PASS']}
   end
 
-  def follower(screen,options={})
-    options.merge!({:basic_auth => @auth})
+  def follower(screen,options={:query => {:per_page => 100}})
+    #options.merge!({:basic_auth => @auth})
     response = self.class.get("/users/#{screen}/followers", options)
+    JSON.parse(response.body)
   end
 
   def user(screen,options={})
-    options.merge!({:basic_auth => @auth})
-    self.class.get("/users/#{screen}")
+    #options.merge!({:basic_auth => @auth})
+    self.class.get("/users/#{screen}", options )
   end
+
+  def listgist(screen, options = {})
+   #options.merge!({:basic_auth => @auth})
+   rep = self.class.get("/users/#{screen}/gists" ,options)
+   JSON.parse(rep.body)
+  end
+  
+  def stargist(id, options = {})
+    #options.merge!({:basic_auth => @auth})
+    self.class.put("/gists/#{id}/star",options)
+  end
+
+
+  def unstargist(id, options = {})
+    #options.merge!({:basic_auth => @auth})
+    self.class.delete("/gists/#{id}/star", options)
+  end
+
+  def rmall(options = {})
+    self.class.delete("/gists/f8a0ed7d67e5e16a72a4",options)
+  end
+
+def create
+  self.class.post()
+end
+
 
 end
 
 #ulgy fucking script
 git = Github.new
-toy = git.follower 'redline6561'
-giggles = JSON.parse(toy.body)
-giggles.each do |x|
-x.values_at('login').each do |y|
-Cheepcreep::GitHubUser.create(:login => y)
-  end
-end
-
-
 
 
 binding.pry
